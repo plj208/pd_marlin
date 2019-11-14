BootleggersROM-Devices/device_google_marlin

marlin/sailfish: reunite extract-files/setup-makefiles scripts
If we use some simple arguments here, we can cut down on some redundant
script.

Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

extract-files.sh
@@ -1,1 +1,54 @@
-marlin/extract-files.sh
+#!/bin/bash
+#
+# Copyright (C) 2017-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -e
+
+VENDOR=google
+
+# Load extractutils and do some sanity checks
+MY_DIR=$PWD
+BOOTLEG_ROOT=$MY_DIR/../../..
+HELPER=$BOOTLEG_ROOT/vendor/bootleggers/build/tools/extract_utils.sh
+DEVICE=$1
+[ -n "$2" ] && SRC=$2 || SRC=adb
+
+if [ ! -f $HELPER ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. $HELPER
+
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+    echo "$0: arguments invalid"
+    echo ""
+    echo "usage: $0 [DEVICE] [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+fi
+
+# Initialize the helper
+setup_vendor $DEVICE $VENDOR $BOOTLEG_ROOT
+
+extract $MY_DIR/device-proprietary-files.txt $SRC
+extract $MY_DIR/$DEVICE/device-proprietary-files-vendor.txt $SRC
+
+# Don't disable MyVerizonServices app
+MVS='<disabled-until-used-preinstalled-carrier-app package="com.verizon.mips.services" />'
+sed -i 's|'"$MVS"'|<!--'"$MVS"'-->|' \
+    $BOOTLEG_ROOT/vendor/$VENDOR/$DEVICE/proprietary/etc/sysconfig/nexus.xml
+
+$MY_DIR/setup-makefiles.sh $DEVICE

setup-makefiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,36 +13,39 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 set -e
 
 VENDOR=google
-DEVICE=sailfish
-
-INITIAL_COPYRIGHT_YEAR=2019
+INITIAL_COPYRIGHT_YEAR=2017
 
 # Load extractutils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+MY_DIR=$PWD
+BOOTLEG_ROOT=$MY_DIR/../../..
+HELPER=$BOOTLEG_ROOT/vendor/bootleggers/build/tools/extract_utils.sh
+DEVICE=$1
 
-BOOTLEG_ROOT="$MY_DIR"/../../../..
-
-HELPER="$BOOTLEG_ROOT"/vendor/bootleggers/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
+if [ ! -f $HELPER ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
 fi
-. "$HELPER"
+. $HELPER
+
+if [ $# != 1 ]; then
+    echo "$0: invalid arguments"
+    echo ""
+    echo "usage: $0 [DEVICE]"
+    exit 1
+fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$BOOTLEG_ROOT"
+setup_vendor $DEVICE $VENDOR $BOOTLEG_ROOT
 
 # Copyright headers and guards
 write_headers
 
 # The standard blobs
-write_makefiles "$MY_DIR"/device-proprietary-files.txt
-write_makefiles "$MY_DIR"/device-proprietary-files-vendor.txt true
+write_makefiles $MY_DIR/device-proprietary-files.txt
+write_makefiles $MY_DIR/$DEVICE/device-proprietary-files-vendor.txt true
 
 # Finish
 write_footers

plj208/pd_marlin

extract-files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,49 +13,42 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 set -e
 
 VENDOR=google
-DEVICE=marlin
 
 # Load extractutils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+MY_DIR=$PWD
+PIXELDUST_ROOT=$MY_DIR/../../..
+HELPER=$PIXELDUST_ROOT/vendor/pixeldust/build/tools/extract_utils.sh
+DEVICE=$1
+[ -n "$2" ] && SRC=$2 || SRC=adb
 
-PIXELDUST_ROOT="$MY_DIR"/../../..
-
-HELPER="$PIXELDUST_ROOT"/vendor/pixeldust/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
+if [ ! -f $HELPER ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
 fi
-. "$HELPER"
+. $HELPER
 
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+    echo "$0: arguments invalid"
     echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo "usage: $0 [DEVICE] [PATH_TO_EXPANDED_ROM]"
     echo ""
     echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
     echo "the device using adb pull."
     exit 1
-  fi
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$PIXELDUST_ROOT"
+setup_vendor $DEVICE $VENDOR $PIXELDUST_ROOT
 
-extract "$MY_DIR"/device-proprietary-files.txt "$SRC"
-extract "$MY_DIR"/device-proprietary-files-vendor.txt "$SRC"
+extract $MY_DIR/device-proprietary-files.txt $SRC
+extract $MY_DIR/$DEVICE/device-proprietary-files-vendor.txt $SRC
 
 # Don't disable MyVerizonServices app
-sed -i 's|<disabled-until-used-preinstalled-carrier-app package="com.verizon.mips.services" />|<!--disabled-until-used-preinstalled-carrier-app package="com.verizon.mips.services" /-->|g'\
-    "$PIXELDUST_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/etc/sysconfig/nexus.xml
+MVS='<disabled-until-used-preinstalled-carrier-app package="com.verizon.mips.services" />'
+sed -i 's|'"$MVS"'|<!--'"$MVS"'-->|' \
+    $PIXELDUST_ROOT/vendor/$VENDOR/$DEVICE/proprietary/etc/sysconfig/nexus.xml
 
-"$MY_DIR"/setup-makefiles.sh
+$MY_DIR/setup-makefiles.sh $DEVICE

setup-makefiles.sh
@@ -13,36 +13,39 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 set -e
 
 VENDOR=google
-DEVICE=marlin
-
 INITIAL_COPYRIGHT_YEAR=2017
 
 # Load extractutils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+MY_DIR=$PWD
+PIXELDUST_ROOT=$MY_DIR/../../..
+HELPER=$PIXELDUST_ROOT/vendor/pixeldust/build/tools/extract_utils.sh
+DEVICE=$1
 
-PIXELDUST_ROOT="$MY_DIR"/../../../..
-
-HELPER="$PIXELDUST_ROOT"/vendor/pixeldust/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
+if [ ! -f $HELPER ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
 fi
-. "$HELPER"
+. $HELPER
+
+if [ $# != 1 ]; then
+    echo "$0: invalid arguments"
+    echo ""
+    echo "usage: $0 [DEVICE]"
+    exit 1
+fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$PIXELDUST_ROOT"
+setup_vendor $DEVICE $VENDOR $PIXELDUST_ROOT
 
 # Copyright headers and guards
 write_headers
 
 # The standard blobs
-write_makefiles "$MY_DIR"/device-proprietary-files.txt
-write_makefiles "$MY_DIR"/device-proprietary-files-vendor.txt true
+write_makefiles $MY_DIR/device-proprietary-files.txt
+write_makefiles $MY_DIR/$DEVICE/device-proprietary-files-vendor.txt true
 
 # Finish
 write_footers