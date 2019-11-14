LineageOS/android_device_google_marlin

marlin: Setup separate extract scripts for marlin and sailfish
When building the vendor img, there are differences
between the 2 so we need to separate them.

Change-Id: Ifc608ebea43d0b0326d67c763cb77a9a41967d6e

marlin/setup-makefiles.sh
@@ -1,0 +1,48 @@
+#!/bin/bash
+#
+# Copyright (C) 2017-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+VENDOR=google
+DEVICE=marlin
+
+INITIAL_COPYRIGHT_YEAR=2017
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+
+# Copyright headers and guards
+write_headers
+
+# The standard blobs
+write_makefiles "$MY_DIR"/device-proprietary-files.txt
+write_makefiles "$MY_DIR"/device-proprietary-files-vendor.txt true
+
+# Finish
+write_footers

plj208/pd_marlin

Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

marlin/setup-makefiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,11 +19,13 @@ set -e
 VENDOR=google
 DEVICE=marlin
 
+INITIAL_COPYRIGHT_YEAR=2017
+
 # Load extractutils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-PIXELDUST_ROOT="$MY_DIR"/../../..
+PIXELDUST_ROOT="$MY_DIR"/../../../..
 
 HELPER="$PIXELDUST_ROOT"/vendor/pixeldust/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
@@ -32,30 +34,15 @@ if [ ! -f "$HELPER" ]; then
 fi
 . "$HELPER"
 
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
-fi
-
 # Initialize the helper
 setup_vendor "$DEVICE" "$VENDOR" "$PIXELDUST_ROOT"
 
-extract "$MY_DIR"/device-proprietary-files.txt "$SRC"
-extract "$MY_DIR"/device-proprietary-files-vendor.txt "$SRC"
+# Copyright headers and guards
+write_headers
 
-# Don't disable MyVerizonServices app
-sed -i 's|<disabled-until-used-preinstalled-carrier-app package="com.verizon.mips.services" />|<!--disabled-until-used-preinstalled-carrier-app package="com.verizon.mips.services" /-->|g'\
-    "$PIXELDUST_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/etc/sysconfig/nexus.xml
+# The standard blobs
+write_makefiles "$MY_DIR"/device-proprietary-files.txt
+write_makefiles "$MY_DIR"/device-proprietary-files-vendor.txt true
 
-"$MY_DIR"/setup-makefiles.sh
+# Finish
+write_footers